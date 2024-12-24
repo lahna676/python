a70908fb87b0daa67d8acd352ccca7ddcaccd636
lahna676 <sabiradoctor1977@gmail.com>
2024-12-24 19:31:12 +0530

    percentage

---
+++ percentage.py
@@ -0,0 +1,8 @@
+print('please enter the marks')
+math=int(input('math'))
+english=int(input('english'))
+scince=int(input('scince'))
+evs=int(input('evs'))
+sum=math+english+scince+evs
+percentage=sum/400*100
+print(percentage)
