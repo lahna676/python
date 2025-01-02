24c2dddddcd189152a3b7dcbcae5640d83094ae9
lahna676 <sabiradoctor1977@gmail.com>
2025-01-02 19:29:51 +0530

    oprators3

---
+++ grading system.py
@@ -0,0 +1,18 @@
+print('enter marks obtired in 5 subjects')
+mark1=int(input)
+mark2=int(input)
+mark3=int(input)
+mark4=int(input)
+mark5=int(input)
+tot=mark1+mark2+mark3+mark4+mark5
+avg=tot/5
+if avg>91 and avg >100:
+ print('your grade is a')
+elif avg>81 and avg >91:
+ print('your grade is b')
+elif avg>71 and avg >81
+ print('your grade is c')
+elif avg>61 and avg >71
+ print('your grade is d')
+else avg>51 and avg >61
+ print('your grade is e')
---
+++ identity oprator.py
@@ -0,0 +1,10 @@
+x=5
+if(type(x)is int):
+ print('true')
+else:
+ print('false')
+x=0.5
+if(type(x)is int):
+ print('true')
+else:
+ print('false')
