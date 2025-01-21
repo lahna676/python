eb69b987fb0b89123c317b033df35cf6ed5fe1ee
lahna676 <sabiradoctor1977@gmail.com>
2025-01-21 19:31:24 +0530

    nested loop

---
+++ character of currence.py
@@ -0,0 +1,10 @@
+string=input('please enter your own word:')
+char=input('please enter your own charecter:')
+i=0
+count=0
+while(i< len (string)):
+ if (string[i]==char):
+  count=count+1
+  i=i+1
+ print('the total number of times',charecter has)
+ occured='',count
