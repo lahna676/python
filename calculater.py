ee2cd8e0d06d54692682db9b432e04b7185934d2
lahna676 <sabiradoctor1977@gmail.com>
2025-01-30 19:16:38 +0530

    function

---
+++ calculater.py
@@ -0,0 +1,8 @@
+def add(P,Q):
+ return P+Q
+def subtract(P,Q):
+ return P-Q
+def multiply(P,Q):
+ return P*Q
+def divition(P,Q):
+ return P%Q
---
+++ whether condition.py
@@ -0,0 +1,6 @@
+def whethercondition():
+ print('the whether is plesent in:',spring)
+ print('the whether is plesent in:',atumn)
+spring='atumn'
+atumn=spring
+whethercondition()
